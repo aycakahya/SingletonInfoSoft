78cfecdfbc4e1e9e44137b0c862c8ac8736af309
kahyaayca <yilmaz@LAPTOP-0MLRAL9H>
2022-09-02 22:12:04 +0300

    first commit

--- SingletonInfoSoft/Controllers/HomeController.cs
+++ SingletonInfoSoft/Controllers/HomeController.cs
@@ -28,5 +28,6 @@ namespace SingletonInfoSoft.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+        //test
     }
 }
